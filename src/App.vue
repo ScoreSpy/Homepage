7eba85934b3742b97ebb3dd8478fcbb233d62350
Ahriana <ahrianadev@gmail.com>
2023-01-18 17:34:47 +0000

    quasar base

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Dark } from 'quasar'
+
+export default defineComponent({
+  name: 'App',
+  setup () {
+    Dark.set(true)
+  }
+})
+</script>
